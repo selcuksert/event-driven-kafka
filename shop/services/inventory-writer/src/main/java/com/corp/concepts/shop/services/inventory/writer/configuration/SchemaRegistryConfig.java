65b9e4ac9d139a3a0d947415fff85a3e2d51d56c
Selcuk SERT <selcuk.sert@gmail.com>
2020-06-26 00:08:22 +0300

    update on services

--- shop/services/inventory-writer/src/main/java/com/corp/concepts/shop/services/inventory/writer/configuration/SchemaRegistryConfig.java
+++ shop/services/inventory-writer/src/main/java/com/corp/concepts/shop/services/inventory/writer/configuration/SchemaRegistryConfig.java
@@ -1,4 +1,4 @@
-package com.corp.concepts.shop.services.inventory.processor.configuration;
+package com.corp.concepts.shop.services.inventory.writer.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.schema.registry.client.ConfluentSchemaRegistryClient;
@@ -8,13 +8,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SchemaRegistryConfig {
+
 	@Bean
 	public SchemaRegistryClient schemaRegistryClient(
 			@Value("${spring.cloud.stream.kafka.binder.producer-properties.schema.registry.url}") String endpoint) {
 		ConfluentSchemaRegistryClient client = new ConfluentSchemaRegistryClient();
 		client.setEndpoint(endpoint);
 		return client;
-
 	}
-
 }
